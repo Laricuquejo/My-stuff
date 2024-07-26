8f10b07f0751d6059413ea93ded795ca7d66bda3
Lari <larissa@cuquejo.org>
2024-07-26 16:31:30 +0100

    updates

--- studies/tla/create-vm.sh
+++ studies/tla/create-vm.sh
@@ -3,8 +3,8 @@
 # Define variables
 export PROJECT_ID="robotic-heaven-430515-g6"
 export ZONE="us-central1-a"
-export INSTANCE_NAME="teste"
-export MACHINE_TYPE="e2-micro"
+read -p "Enter the Instance Name: " INSTANCE_NAME
+read -p "Enter the Machine Type (e.g., e2-micro): " MACHINE_TYPE
 export IMAGE="projects/debian-cloud/global/images/debian-12-bookworm-v20240709"
 export NETWORK_TIER="STANDARD"
 export STACK_TYPE="IPV4_ONLY"
@@ -12,7 +12,7 @@ export SUBNET="default"
 export SERVICE_ACCOUNT="309166871037-compute@developer.gserviceaccount.com"
 export SCOPES="https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append"
 export DISK_TYPE="projects/robotic-heaven-430515-g6/zones/us-central1-a/diskTypes/pd-balanced"
-export DISK_SIZE="10"
+read -p "Enter the Disk Size (in GB, e.g., 10): " DISK_SIZE
 
 # Authenticate and set the project (if not already authenticated)
 gcloud auth login
